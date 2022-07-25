e4fa4c04c628c52f8d0238585bfc3aa660312a3e
giabao1511 <giabao712411@gmail.com>
2022-07-25 21:15:00 +0700

    fix bug

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,22 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config({ path: "../.env" });
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/user");
-const connectDB = require("./configs/db");
+const authRoute = require("./routes/auth.js");
+const userRoute = require("./routes/user.js");
+const connectDB = require("./configs/db.js");
 connectDB();
 const app = express();
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "Bao Đẹp trai ",
+    timestamp: Date.now(),
+  };
+  return res.send(healthcheck);
+});
 // ROUTES
 app.use("/v1/auth", authRoute);
 app.use("/v1/user", userRoute);
